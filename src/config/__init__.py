c30594f3396584fd1ad5e8a98aa3f564b6857ec0
fuy <326982133@qq.com>
2025-08-01 22:59:03 +0800

    Initial commit

--- src/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-配置模块
-提供配置加载、验证和管理功能
-"""
-
-from src.config.config_manager import ConfigManager
-from src.config.settings import DEFAULT_CONFIG, DISCLAIMER
-
-__all__ = ['ConfigManager', 'DEFAULT_CONFIG', 'DISCLAIMER'] 
--- src/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UI模块
-提供用户界面和通知功能
-"""
-
-from src.ui.notification_manager import NotificationManager
-
-__all__ = ['NotificationManager'] 
